5c2b96cd2b0ce91514b0365bfdf8da9c2ccc391f
Kongisa <217010951@stud.cut.ac.za>
2021-10-15 14:24:50 +0200

    added again

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context)
   {
     return MaterialApp(
-      title: "Welcome to gift me",
+      title: "Welcome to gift me again",
       home: AddDonation(),
     );
   }
